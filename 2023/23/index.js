1e90968676bbca287e79823992f9086aedafa26f
Jonny Buchanan <jonathan.buchanan@gmail.com>
2023-12-24 09:45:03 +1000

    2023/23 part 2

--- 2023/23/index.js
+++ 2023/23/index.js
@@ -50,13 +50,13 @@ while (paths.length > 0) {
  * @param {string} dir
  * @return {[[number, number], string][]}
  */
-function getPossibleMoves(pos, dir) {
+function getPossibleMoves(pos, dir, ignoreArrows = false) {
   /** @type {[[number, number], string][]} */
   let possible = []
 
   // If the current space is an arrow we must go in that direction
   let gridDir = grid.at(pos)
-  if (dirs[gridDir]) {
+  if (!ignoreArrows && dirs[gridDir]) {
     // Unless we came from the opposite direction
     if (gridDir != oppositeDirs[dir]) {
       possible.push([add(pos, dirs[gridDir]), gridDir])
@@ -90,4 +90,63 @@ console.log(
 console.log()
 
 console.log('Part 2')
-console.log('answer:')
+
+let start = '1,0'
+let end = `${grid.width - 2},${grid.height - 1}`
+let junctions = new Set([start, end])
+for (let x = 0; x < grid.width; x++) {
+  for (let y = 0; y < grid.height; y++) {
+    if (grid.at([x, y]) == '#') continue
+    let possibles = []
+    for (let delta of Object.values(dirs)) {
+      let pos = add([x, y], delta)
+      if (grid.contains(pos) && grid.at(pos) != '#') {
+        possibles.push(grid.at(pos))
+      }
+    }
+    if (possibles.length > 2) {
+      junctions.add([x, y].join(','))
+    }
+  }
+}
+
+let junctionDistances = new Map()
+for (let junction of junctions) {
+  let pos = junction.split(',').map(Number)
+  let distances = new Map()
+  /** @type {[[number, number], string, number][]} */
+  let paths = [[pos, '', 0]]
+  while (paths.length > 0) {
+    /** @type {[[number, number], string, number][]} */
+    let next = []
+    for (let [pos, dir, steps] of paths) {
+      for (let [nextPos, nextDir] of getPossibleMoves(pos, dir, true)) {
+        if (junctions.has(nextPos.join(','))) {
+          distances.set(nextPos.join(','), steps + 1)
+        } else {
+          next.push([nextPos, nextDir, steps + 1])
+        }
+      }
+    }
+    paths = next
+  }
+  junctionDistances.set(junction, distances)
+}
+
+let longest = 0
+function findLongest(junction, seen, steps) {
+  if (junction == end) {
+    if (steps > longest) {
+      longest = steps
+    }
+    return
+  }
+
+  for (let [next, distance] of junctionDistances.get(junction).entries()) {
+    if (seen.has(next)) continue
+    findLongest(next, new Set([...seen, next]), steps + distance)
+  }
+}
+
+findLongest(start, new Set([start]), 0)
+console.log('answer:', longest)
