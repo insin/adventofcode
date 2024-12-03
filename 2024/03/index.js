f7df4ea63c1202f30e0b8a8d66d4bbbeeee8a071
Jonny Buchanan <jonathan.buchanan@gmail.com>
2024-12-03 15:52:14 +1000

    Added 2024/03

---
+++ 2024/03/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+
+let inputs = ['example1', 'example2', 'input'].map((file) =>
+  fs.readFileSync(`${file}.txt`, 'utf-8')
+)
+let program = inputs.at(process.argv[2] == 'test' ? 1 : -1)
+
+console.log('Part 1')
+let answer1 = 0
+for (let match of program.matchAll(/mul\((\d+),(\d+)\)/g)) {
+  answer1 += Number(match[1]) * Number(match[2])
+}
+console.log('answer:', answer1)
+console.log()
+
+console.log('Part 2')
+let answer2 = 0
+let active = true
+for (let match of program.matchAll(
+  /(mul)\((\d+),(\d+)\)|(do)\(\)|(don't)\(\)/g
+)) {
+  if (match[0] == 'do()') {
+    active = true
+  } else if (match[0] == "don't()") {
+    active = false
+  } else if (active) {
+    answer2 += Number(match[2]) * Number(match[3])
+  }
+}
+console.log('answer:', answer2)
