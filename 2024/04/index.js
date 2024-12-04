a708fe43ad26b5ed77f39696686d7a5afb1e8930
Jonny Buchanan <jonathan.buchanan@gmail.com>
2024-12-05 03:32:45 +1000

    Annoying refactor into maps and filters and whatnot

--- 2024/04/index.js
+++ 2024/04/index.js
@@ -7,24 +7,23 @@ let inputs = ['example', 'input'].map((file) =>
 let grid = getGrid(inputs.at(process.argv[2] == 'test' ? 0 : -1))
 
 console.log('Part 1')
-let answer1 = 0
-for (let {value, pos} of grid) {
-  if (value != 'X') continue
-  for (let delta of deltas) {
-    if (
-      grid.get(add(pos, delta, 1)) == 'M' &&
-      grid.get(add(pos, delta, 2)) == 'A' &&
-      grid.get(add(pos, delta, 3)) == 'S'
-    ) {
-      answer1++
-    }
-  }
-}
-console.log('answer:', answer1)
+console.log(
+  'answer:',
+  Array.from(grid)
+    .filter(({value}) => value == 'X')
+    .map(({pos}) =>
+      deltas.filter(
+        (delta) =>
+          grid.get(add(pos, delta, 1)) == 'M' &&
+          grid.get(add(pos, delta, 2)) == 'A' &&
+          grid.get(add(pos, delta, 3)) == 'S'
+      )
+    )
+    .flat().length
+)
 console.log()
 
 console.log('Part 2')
-let answer2 = 0
 /** @type {[number, number][][]} */
 let oppositeDiagonals = [
   [
@@ -36,19 +35,17 @@ let oppositeDiagonals = [
     [-1, 1], // left down
   ],
 ]
-for (let {value, pos} of grid) {
-  if (value != 'A') continue
-  let mas = true
-  for (let [delta1, delta2] of oppositeDiagonals) {
-    let values = new Set([
-      grid.get(add(pos, delta1)),
-      grid.get(add(pos, delta2)),
-    ])
-    if (!(values.has('M') && values.has('S'))) {
-      mas = false
-      break
-    }
-  }
-  if (mas) answer2++
-}
-console.log('answer:', answer2)
+console.log(
+  'answer:',
+  Array.from(grid).filter(
+    ({value, pos}) =>
+      value == 'A' &&
+      oppositeDiagonals.every(([delta1, delta2]) => {
+        let values = new Set([
+          grid.get(add(pos, delta1)),
+          grid.get(add(pos, delta2)),
+        ])
+        return values.has('M') && values.has('S')
+      })
+  ).length
+)
