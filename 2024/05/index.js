054e50b9d1b8d766515fef9a9f3861620c7d2ae1
Jonny Buchanan <jonathan.buchanan@gmail.com>
2024-12-05 18:15:36 +1000

    Oops, the whole afters thing was completely redundant

--- 2024/05/index.js
+++ 2024/05/index.js
@@ -9,25 +9,17 @@ let [rulesInput, updatesInput] = inputs
   .split('\n\n')
 
 let beforeRules = new Map()
-let afterRules = new Map()
 for (let [n1, n2] of rulesInput.split('\n').map(nums)) {
   beforeRules.set(n1, (beforeRules.get(n1) ?? new Set()).add(n2))
-  afterRules.set(n2, (afterRules.get(n2) ?? new Set()).add(n1))
 }
 let updates = updatesInput.split('\n').map(nums)
 
 function isCorrect(update) {
-  let afters = new Set(update.slice(1))
   let befores = new Set()
   for (let n of update) {
-    if (
-      (beforeRules.has(n) &&
-        beforeRules.get(n).intersection(befores).size > 0) ||
-      (afterRules.has(n) && afterRules.get(n).intersection(afters).size > 0)
-    )
+    if (beforeRules.has(n) && beforeRules.get(n).intersection(befores).size > 0)
       return false
     befores.add(n)
-    afters.delete(n)
   }
   return true
 }
@@ -51,7 +43,6 @@ console.log(
       (update) =>
         update.sort((a, b) => {
           if (beforeRules.has(a) && beforeRules.get(a).has(b)) return -1
-          if (afterRules.has(a) && afterRules.get(a).has(b)) return 1
         })[Math.floor(update.length / 2)]
     )
     .reduce((acc, num) => acc + num, 0)
