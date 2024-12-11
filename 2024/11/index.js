97865a8325bb0bcceefe5644ad1fe26ce5b14862
Jonny Buchanan <jonathan.buchanan@gmail.com>
2024-12-11 17:48:12 +1000

    2024/11 Part 2

--- 2024/11/index.js
+++ 2024/11/index.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const {nums} = require('../../utils')
+const {nums, sum} = require('../../utils')
 let inputs = ['example', 'input'].map((file) =>
   fs.readFileSync(`${file}.txt`, 'utf-8')
 )
@@ -7,9 +7,10 @@ let inputs = ['example', 'input'].map((file) =>
 let stones = nums(inputs.at(process.argv[2] == 'test' ? 0 : -1))
 
 console.log('Part 1')
+let currentStones = stones.slice()
 for (let blinks = 1; blinks <= 25; blinks++) {
   let nextStones = []
-  for (let stone of stones) {
+  for (let stone of currentStones) {
     if (stone == 0) {
       nextStones.push(1)
     } else if (String(stone).length % 2 == 0) {
@@ -20,10 +21,29 @@ for (let blinks = 1; blinks <= 25; blinks++) {
       nextStones.push(stone * 2024)
     }
   }
-  stones = nextStones
+  currentStones = nextStones
 }
-console.log('answer:', stones.length)
+console.log('answer:', currentStones.length)
 console.log()
 
 console.log('Part 2')
-console.log('answer:')
+/** @type {Map<number, number>} */
+let counts = new Map(stones.map((stone) => [stone, 1]))
+for (let blinks = 1; blinks <= 75; blinks++) {
+  let nextCounts = new Map()
+  for (let [stone, count] of counts) {
+    if (stone == 0) {
+      nextCounts.set(1, (nextCounts.get(1) ?? 0) + count)
+    } else if (String(stone).length % 2 == 0) {
+      let str = String(stone)
+      let stone1 = Number(str.substring(0, str.length / 2))
+      let stone2 = Number(str.slice(str.length / 2))
+      nextCounts.set(stone1, (nextCounts.get(stone1) ?? 0) + count)
+      nextCounts.set(stone2, (nextCounts.get(stone2) ?? 0) + count)
+    } else {
+      nextCounts.set(stone * 2024, (nextCounts.get(stone * 2024) ?? 0) + count)
+    }
+  }
+  counts = nextCounts
+}
+console.log('answer:', sum(Array.from(counts.values())))
